e7ea08b545cd5003b0715dc6b74ee72bc8fcd48f
Logik.tv <131388375+flamelogik@users.noreply.github.com>
2023-04-20 17:22:48 -0700

    Add files via upload

---
+++ INSTALL_FLAME264.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Clone the repository
+git clone https://github.com/flamelogik/flame264.git
+
+# Copy the Python file to /opt/Autodesk/shared/python
+cp flame264/*.py /opt/Autodesk/shared/python
+
+# Copy the XML files to /opt/Autodesk/shared/export/presets/movie_file/
+cp flame264/*.xml /opt/Autodesk/shared/export/presets/movie_file/
+
+# Copy the cdxprof files to /opt/Autodesk/mediaconverter/2023.2/profiles/Quicktime/video/H264/
+cp flame264/*.cdxprof /opt/Autodesk/mediaconverter/2023.2/profiles/Quicktime/video/H264/
